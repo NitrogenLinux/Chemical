5dbebc927959a6bbf5ab7313c0668f8dbc34bb7e
tekq <81379378+tekq@users.noreply.github.com>
2022-03-30 00:10:11 +0300

    Update to the new Void Linux-based installer

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ if os.geteuid() != 0:
     exit("You must run chemical as root!")
 
 # Check for internet
-if os.system("ping archlinux.org -c 1") != 0:
+if os.system("ping voidlinux.org -c 1") != 0:
     exit("You must be connected to the internet to use chemical.")
 
 if os.path.isdir("/sys/firmware/efi/efivars/") is True:
@@ -22,7 +22,7 @@ else:
     atomic = False
 
 def install():
-    os.system("pacman -Sy git wget curl --noconfirm")
+    os.system("xbps-install -Sy git wget curl")
     os.system("clear")
     if atomic is False:
         print("Chemical 1.0")
@@ -103,8 +103,8 @@ def install():
         os.system("mount /dev/" + disk + efi_part + " /mnt/boot/EFI")
 
     print("Installing Nitrogen Base")
-    os.system("pacstrap /mnt base linux-lts linux-firmware base-devel sof-firmware python btrfs-progs")
-    os.system("genfstab -U /mnt >> /mnt/etc/fstab")
+    os.system("for dir in dev proc sys; mkdir /mnt/$dir ; do mount --rbind /$dir /mnt/$dir; done")
+    os.system("xbps-install -Sy -R https://alpha.de.repo.voidlinux.org/current -r /mnt base-system grub os-prober nano btrfs-progs")
     print("Configuring Nitrogen")
     print("Select Region")
     region_right = False
@@ -128,21 +128,18 @@ def install():
 
     os.system("ln -s /mnt/usr/share/zoneinfo/" + region + "/" + city + " /mnt/etc/localtime")
     os.system("echo LANG=en_US.UTF-8 >> /mnt/etc/locale.conf")    
-    os.system("echo 'en_US.UTF-8 UTF-8' >> /mnt/etc/locale.gen")
-    os.system("arch-chroot /mnt/ locale-gen")
-    os.system("arch-chroot /mnt/ hwclock --systohc")
 
     print("Select hostname(empty for default)")
     hostname = input("Hostname: ")
     if len(hostname) == 0:
-        os.system("echo nitrogen > /mnt/etc/hostname")
+        os.system("echo Nitrogen > /mnt/etc/hostname")
     else:
         os.system("echo " + hostname + " > /mnt/etc/hostname")
 
     print("Password for root")
     correct_passwd = False
     while correct_passwd is False:
-        c_passwd_answer = os.system("arch-chroot /mnt/ passwd root")
+        c_passwd_answer = os.system("chroot /mnt/ passwd root")
         if c_passwd_answer == 0:
             correct_passwd = True
             pass
@@ -150,10 +147,10 @@ def install():
 
     print("Creating a user")
     username = input("New user's name: ")
-    os.system("arch-chroot /mnt/ useradd -m -G wheel " + username)
+    os.system("chroot /mnt/ useradd -m -G wheel " + username)
     correct_passwd = False
     while correct_passwd is False:
-        if os.system("arch-chroot /mnt/ passwd " + username) == 0:
+        if os.system("chroot /mnt/ passwd " + username) == 0:
             correct_passwd = True
             pass
 
@@ -164,54 +161,54 @@ def install():
     print("3. Intel")
     print("4. Other/VM")
 
-    gpu = int(input("GPU Manufacturer: "))
-    if gpu == 1:
-        os.system("pacstrap /mnt nvidia-lts")
-    elif gpu == 2:
-        os.system("pacstrap /mnt mesa")
-    elif gpu == 3:
-        os.system("pacstrap /mnt mesa xf86-video-intel")
-    elif gpu == 4:
-        print("Virtual Machines:")
-        print("1. VMWare")
-        print("2. KVM")
-        print("3. None")
+# TODO: bring these to Void Linux
 
-        vm_gpu = int(input("GPU Manufacturer: "))
-        if vm_gpu == 1:
-            print("Also installing VMmouse")
-            os.system("pacstrap /mnt xf86-video-vmware xf86-input-vmmouse")
-        elif vm_gpu == 2:
-            os.system("pacstrap /mnt xf86-video-qxl")
-        elif vm_gpu == 3:
-            pass
+#    gpu = int(input("GPU Manufacturer: "))
+#     if gpu == 1:
+#         os.system("pacstrap /mnt nvidia-lts")
+#     elif gpu == 2:
+#         os.system("pacstrap /mnt mesa")
+#     elif gpu == 3:
+#         os.system("pacstrap /mnt mesa xf86-video-intel")
+#     elif gpu == 4:
+#         print("Virtual Machines:")
+#         print("1. VMWare")
+#         print("2. KVM")
+#         print("3. None")
+#
+#         vm_gpu = int(input("GPU Manufacturer: "))
+#         if vm_gpu == 1:
+#             print("Also installing VMmouse")
+#             os.system("pacstrap /mnt xf86-video-vmware xf86-input-vmmouse")
+#         elif vm_gpu == 2:
+#             os.system("pacstrap /mnt xf86-video-qxl")
+#         elif vm_gpu == 3:
+#             pass
 
     print("Would you like to install Laptop-specific packages?")
     laptop = input("Y/n ")
     if str(laptop) in ["y", "Y"]:
-        os.system("pacstrap /mnt tlp powertop")
-        os.system("arch-chroot /mnt/ systemctl --enable tlp")
+        os.system("xbps-install -Sy -R https://alpha.de.repo.voidlinux.org/current -r /mnt tlp powertop")
+        os.system("chroot /mnt/ systemctl --enable tlp")
     else:
         pass
 
     print("Installing and Configuring Boot Loader")
     if efi is True:
-        os.system("pacstrap /mnt efibootmgr dosfstools os-prober mtools grub") # UEFI Specific Packages and grub
-        os.system("arch-chroot /mnt/ grub-install --target=x86_64-efi --bootloader-id=Nitrogen") # GRUB UEFI Installation
+        os.system("chroot /mnt/ grub-install --target=x86_64-efi --bootloader-id=Nitrogen") # GRUB UEFI Installation
         import time
     else:
         print("Installing Grub")
-        os.system("pacstrap /mnt grub") # install GRUB
-        os.system("arch-chroot /mnt/ grub-install --target=i386-pc /dev/" + disk) # Grub BIOS installation
+        os.system("chroot /mnt/ grub-install --target=i386-pc /dev/" + disk) # Grub BIOS installation
 
-    os.system("arch-chroot /mnt/ grub-mkconfig -o /boot/grub/grub.cfg") # Create Grub Configuration
+    os.system("chroot /mnt/ xbps-reconfigure -fa")
 
     print("Configuring Nitrogen pt 2")
     os.system("curl https://raw.githubusercontent.com/NitrogenLinux/chemical/main/os-release > /mnt/etc/os-release")
     os.system("curl https://raw.githubusercontent.com/NitrogenLinux/chemical/main/os-release > /mnt/usr/lib/os-release")
 
     print("Installing Elements")
-    os.system("pacstrap /mnt wget git python python-pip")
+    os.system("xbps-install -Sy -R https://alpha.de.repo.voidlinux.org/current -r /mnt wget git python python-pip")
     os.system("wget https://github.com/NitrogenLinux/elements/raw/stable/lmt") # Download Elements
     os.system("mv -v lmt /mnt/usr/bin")
     os.system("mkdir -p /mnt/etc/elements/repos/")
@@ -221,7 +218,7 @@ def install():
     os.system("mv -v search* /mnt/etc/elements/")
     os.system("chmod a+x /mnt/usr/bin/lmt")
     os.system("chmod a+x /mnt/etc/elements/search*")
-    os.system("arch-chroot /mnt/ pip install python-Levenshtein colorama fuzzywuzzy")
+    os.system("chroot /mnt/ pip install colorama requests")
 
     if atomic is True:
         print("Will you be using iwd NetworkManager or wpa_supplicant?")
@@ -230,23 +227,22 @@ def install():
         print("3. wpa_supplicant")
         network_supplier = int(input())
         if network_supplier == 1:
-            os.system("pacstrap /mnt iwd")
-            os.system("arch-chroot /mnt/ systemctl enable iwd")
+            os.system("xbps-install -Sy -R https://alpha.de.repo.voidlinux.org/current -r /mnt iwd")
+            os.system("chroot /mnt/ systemctl enable iwd")
         elif network_supplier == 2:
-            os.system("pacstrap /mnt networkmanager")
-            os.system("arch-chroot /mnt/ systemctl enable NetworkManager")
+            os.system("xbps-install -Sy -R https://alpha.de.repo.voidlinux.org/current -r /mnt networkmanager")
+            os.system("chroot /mnt/ systemctl enable NetworkManager")
         elif network_supplier == 3:
-            os.system("pacstrap /mnt wpa_supplicant wpa_cli")
-            os.system("arch-chroot /mnt/ systemctl enable wpa_supplicant")
+            os.system("xbps-install -Sy -R https://alpha.de.repo.voidlinux.org/current -r /mnt wpa_supplicant wpa_cli")
+            os.system("chroot /mnt/ systemctl enable wpa_supplicant")
 
     else:
         print("Installing desktop environment")
-        os.system("pacstrap /mnt networkmanager gnome gdm")
-        os.system("arch-chroot /mnt/ systemctl enable gdm")
-        os.system("arch-chroot /mnt/ systemctl enable NetworkManager")
+        os.system("xbps-install -Sy -R https://alpha.de.repo.voidlinux.org/current -r /mnt networkmanager gnome gdm")
+        os.system("chroot /mnt/ systemctl enable gdm")
+        os.system("chroot /mnt/ systemctl enable NetworkManager")
 
 
-    print("")
     print("")
     print("")
 
