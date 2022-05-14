823f56305d19070ee330e1a4d887b088518c87b0
tekq <81379378+tekq@users.noreply.github.com>
2022-05-14 16:25:06 +0300

    fix locales

--- main.py
+++ main.py
@@ -131,7 +131,8 @@ def install():
             pass
 
     os.system("ln -s /mnt/usr/share/zoneinfo/" + region + "/" + city + " /mnt/etc/localtime")
-    os.system("echo LANG=en_US.UTF-8 >> /mnt/etc/default/libc-locales")
+    os.system('echo "LANG=en_US.UTF-8" > /etc/locale.conf')
+    os.system('echo "en_US.UTF-8 UTF-8" >> /etc/default/libc-locales')
     os.system("xbps-reconfigure -f glibc-locales")
 
     print("Select hostname(empty for default)")
